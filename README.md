dee252992453e4e205d4de1b58d29afccb529867
Giraut <>
2025-07-13 09:35:18 +0300

    First release

---
+++ README.md
@@ -0,0 +1,104 @@
+# nest_sway.sh
+### Version 1.0
+
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Why this script is needed](#Why-this-script-is-needed)
+* [License](#License)
+
+Shell script to start a nested Sway (Wayland) session as another user.
+
+![Nested Sway session](images/nested_sway_session.png)
+
+
+
+## Installation
+
+- Install [filterway](https://github.com/andrewbaxter/filterway):
+
+  ```console
+  git clone https://github.com/andrewbaxter/filterway
+  cd filterway
+  cargo build
+  sudo install -m 755 target/debug/filterway /usr/bin
+  ```
+
+- Copy the `nest_sway.sh` script somewhere in your PATH. I personally put all my scripts in a `~/scripts/` directory:
+
+  ```console
+  install -D -m 755 nest_sway.sh ~/scripts/nest_sway.sh
+  ```
+
+- By default, `nest_sway.sh` uses [pkexec](https://allthings.how/use-pkexec-sudo-alternative-run-commands-root-linux/) to become root. If you have a graphical polkit agent such as [LXPolkit](https://blog.lxde.org/2010/03/28/lxpolkit-simple-policykit-authentication-agent/), it allows you to enter your password in a popup window and start the nested Sway session without a terminal.
+
+  If you'd rather use the traditional `sudo` command, edit the script and change the `RUN_AS_ROOT_CMD` variable.
+
+- If you use the script with `pkexec` and you'd like to launch a nested session as a particular user from your favorite application launcher, you can create a .desktop file in your local `~/.local/share/applications/` directory.
+
+  For example, to launch a session as user `test`, create `~/.local/share/applications/test-desktop.desktop` with the following content or similar:
+
+  ```
+  [Desktop Entry]
+  Name=test desktop
+  Exec=nest_sway.sh test
+  Icon=gnome-remote-desktop
+  Type=Application
+  Keywords=sway;nest;nested;
+  ```
+
+
+
+## Usage
+
+### From the terminal
+
+  ```console
+  nest_sway.sh <nested session user>
+  ```
+
+  If you use the script with `pkexec`, your graphical polkit agent will appear and ask you for your password to become root:
+
+  ![Polkit agent popup window](images/polkit_agent_popup_window.png)
+
+  Otherwise `sudo` will ask you in the terminal if needed.
+
+### From your application launcher with a .desktop file
+
+  If you crated a .desktop file as described above, simply launch the application from your application launcher:
+
+  ![Application launcher window](images/application_launcher_window.png)
+
+
+
+
+## Why this script is needed
+
+Sway - or more precisely [wlroots](https://github.com/swaywm/wlroots) - can already run nested natively without any modification. You can test that by opening a terminal and typing `sway` in it: you'll get a second, identical desktop inside your current one, without needing any script.
+
+The problems come when you want to run another user's desktop within yours, for the following reasons:
+
+1. Wayland makes the incredibly restrictive assumption that the Wayland compositor and clients always run as the same user, and therefore puts the Wayland socket in the user's XDG_RUNTIME_DIR (usually `/run/user/<userid>/`).
+
+   That's a problem if you want a Wayland application running as another user to connect to that Wayland socket, because other users can't access your XDG_RUNTIME_DIR. And you really don't want to open it up to other users just to be able to access the socket because it's full of sensitive files pertaining to your running session.
+
+   Moreover, since XDG_RUNTIME_DIR is usually a mounted tmpfs, you can't symlink the socket outside the directory either because sockets can't be symlinked across filesystems in Linux.
+
+   In other words, as usual, Wayland makes it extra difficult to do something simple for no good reason.
+
+2. Sway requires a full login environment - and particularly XDG_RUNTIME_DIR - to be set in the environment, which usually implies that it should also be setup and mounted in `/run/user`.
+
+   Unfortunately, you can't just sudo into the account you want to run your nested Sway desktop as and start Sway because PAM explicitely doesn't set XDG when su'ing or sudo'ing, and doing it manually is a recipe for problems.
+
+To solve **1**, we use the clever piece of software [filterway](https://github.com/andrewbaxter/filterway), which conveniently solves two problems:
+
+- It acts as a sort of gateway: it connects to a Wayland socket on one side, creates its own socket on the other side and links the two. This functionality is used to expose the primary Wayland socket securely without compromising XDG_RUNTIME_DIR. It wasn't really made to do that but conveniently, it fits the bill perfectly.
+
+- It replaces the app ID of the top Wayland client that connects to it - which is really its main party trick. In this particular use case, that's useful to track the state of the nested Sway session in the primary session's tree and figure out when the window is closed.
+
+To solve **2**, we use `systemd-run` to setup the target user's environment as if it was a full login, then run Sway with the correct setup to connect to the primary Wayland display's socket.
+
+
+
+## License
+
+MIT
---
+++ nest_sway.sh
@@ -0,0 +1,183 @@
+#!/bin/sh
+
+# Method to use to become root:
+# - sudo: ubiquitous but requires running in a terminal to enter the password if
+#         the sudoer is not set passwordless
+# - pkexec: requires a graphical polkit agent
+#RUN_AS_ROOT_CMD=sudo
+RUN_AS_ROOT_CMD=pkexec
+
+# Are we running as a normal user?
+if [ $(whoami) != root ]; then
+
+  # Make sure we run in Wayland
+  if [ ! "${WAYLAND_DISPLAY}" ]; then
+    echo "$0 must be run in a Wayland environment"
+    exit
+  fi
+  
+  # Make sure we run in Sway
+  if [ ! "${SWAYSOCK}" ]; then
+    echo "$0 must be run in a Sway session"
+    exit
+  fi
+  
+  # Get the nested session's user as first argument
+  if [ ! "$1" ]; then
+    echo "Usage: $0 username"
+    exit
+  fi
+  NUSER=$1
+  
+  # Make sure the nested session's user exists
+  if ! grep -q "^${NUSER}:" /etc/passwd; then
+    echo "User ${NUSER} doesn't exist"
+    exit
+  fi
+  
+  # Make sure filterway is installed
+  if ! which -s filterway; then
+    echo "filterway not found in the PATH."
+    echo "Please install if from https://github.com/andrewbaxter/filterway"
+    exit
+  fi
+  
+  # Get a unique ID for this nested session
+  UUID=$(uuidgen)
+  
+  # Figure out where our Wayland socket is and make sure it exists
+  if echo ${WAYLAND_DISPLAY} | grep -q "^/"; then 
+    RSOCKPATH=${WAYLAND_DISPLAY}
+  else
+    RSOCKPATH=${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY}
+  fi
+  if ! [ -S ${RSOCKPATH} ]; then 
+    echo -n "Socket file ${RSOCKPATH} for this Wayland display "
+    echo "\"${WAYLAND_DISPLAY}\" doesn't exist!?"
+    exit
+  fi
+  
+  # Unique nested session's Wayland display name
+  NWDISPLAY=wayland-nested-${UUID}
+  
+  # Unique filespec for the nested session's Wayland socket
+  NSOCKPATH=/tmp/${NWDISPLAY}
+  
+  # Unique filespec for the nested Sway socket
+  NSWAYSOCK=/tmp/sway-nested-ipc.${NUSER}.${UUID}.sock
+  
+  # Run filterway in the background to rename the nested session's app ID, but
+  # more importantly to expose our private Wayland socket located in
+  # XDG_RUNTIME_DIR, because XDG_RUNTIME_DIR is most likely a tmpfs-mounted
+  # directory and changing its permissions to allow a different user tp access
+  # the socket would compromise the directory
+  rm -f ${NSOCKPATH}
+  ID="Nested Sway - ${NUSER} ($UUID)"
+  filterway --upstream ${RSOCKPATH} --downstream ${NSOCKPATH} --app-id "${ID}" &
+  FILTERWAY_PID=$!
+  
+  # Wait until filterway has created the socket and associated lock files for
+  # the nested session
+  RETRY=3
+  while [ ${RETRY} -gt 0 ] && \
+	! ( [ -S ${NSOCKPATH} ] && [ -f ${NSOCKPATH}.lock ] ); do
+    sleep 1
+    RETRY=$((RETRY-1))
+  done
+  
+  # If filterway somehow didn't start, try to kill it and clean up its files
+  # for good measure
+  if [ ${RETRY} = 0 ]; then
+    kill ${FILTERWAY_PID}
+    rm -f ${NSOCKPATH} ${NSOCKPATH}.lock
+  fi
+  
+  # Fix up the permissions of the socket and associated lock files for the
+  # nested session so it's only accessible to the owner
+  chmod 600 ${NSOCKPATH} ${NSOCKPATH}.lock
+  
+  # Re-run ourselves as root to perform the rest of the operations to spawn the
+  # nested session
+  VARFILE=${XDG_RUNTIME_DIR}/$(echo $UUID | cut -d- -f1).nested_sway_vars
+
+  echo "NUSER=${NUSER}" > ${VARFILE}
+  echo "NWDISPLAY=${NWDISPLAY}" >> ${VARFILE}
+  echo "UUID=${UUID}" >> ${VARFILE}
+  echo "NSOCKPATH=${NSOCKPATH}" >> ${VARFILE}
+  echo "SWAYSOCK=${SWAYSOCK}" >> ${VARFILE}
+  echo "NSWAYSOCK=${NSWAYSOCK}" >> ${VARFILE}
+  echo "FILTERWAY_PID=${FILTERWAY_PID}" >> ${VARFILE}
+
+  if ! ${RUN_AS_ROOT_CMD} $0 ${VARFILE}; then
+
+    # If pkexec failed because the user cancelled the login or because the root
+    # part of the script failed for whatever reason, kill filterway and remove
+    # its socket and socket lock files ourselves
+    kill ${FILTERWAY_PID}
+    rm -f ${NUSER}: ${NSOCKPATH} ${NSOCKPATH}.lock
+
+  fi
+
+  # Remove the variables file
+  rm ${VARFILE}
+
+# We run as root
+else
+
+  # Source the variables file we need created in the non-root part
+  if [ ! "$1" ]; then
+    echo "$0 (running as root): missing variable file needed"
+    exit 1
+  fi
+  . $1
+
+  # Check that we were passed all the variables we need
+  for VAR in NUSER NWDISPLAY UUID NSOCKPATH SWAYSOCK NSWAYSOCK FILTERWAY_PID; do
+    eval "VAL=\${$VAR}"
+    if [ ! "${VAL}" ]; then
+      echo "$0 (running as root): missing ${VAR} variable"
+      exit 1
+    fi
+  done
+
+  # Give the socket and associated lock files to the nested session's user
+  chown ${NUSER}: ${NSOCKPATH} ${NSOCKPATH}.lock
+  
+  # Script to run as the nested session's user: clean up stale symlink in
+  # XDG_RUNTIME_DIR then run Sway
+  CMD='[ "${XDG_RUNTIME_DIR}" ] &&
+	(find ${XDG_RUNTIME_DIR} -maxdepth 1 \
+				-name "wayland-nested-*" \
+				-xtype l \
+				-exec rm -f {} \; || true) &&
+	rm -f ${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY} &&
+	ln -s ${NSOCKPATH} ${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY} &&
+	unset NSOCKPATH &&
+	sway'
+
+  # Run the command as the nested session's user
+  systemd-run -E WAYLAND_DISPLAY=${NWDISPLAY} \
+		-E NSOCKPATH=${NSOCKPATH} \
+		-E SWAYSOCK=${NSWAYSOCK} \
+		-PM ${NUSER}@ --user /bin/sh -c "${CMD}" &
+
+  # Wait for the Sway container to appear within 3 seconds after starting Sway,
+  # then wait for it to disappear for more than 5 seconds afterwards
+  COUNTDOWN=3
+  while [ ${COUNTDOWN} -gt 0 ]; do
+    if SWAYSOCK=${SWAYSOCK} swaymsg -t get_tree | \
+	grep -q "app_id.*${UUID}"; then
+      COUNTDOWN=5
+    fi
+    sleep 1
+    COUNTDOWN=$((COUNTDOWN-1))
+  done
+  
+  # Stop the nested Sway
+  SWAYSOCK=${NSWAYSOCK} swaymsg exit
+
+  # Kill filterway and remove its socket and socket lock files
+  kill ${FILTERWAY_PID}
+  rm -f ${NUSER}: ${NSOCKPATH} ${NSOCKPATH}.lock
+
+fi
